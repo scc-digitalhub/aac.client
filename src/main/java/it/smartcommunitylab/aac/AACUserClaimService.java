d54b12478af315af49d8d131aaddd1d688de63ab
kazhamiakin <kazhamiakin@gmail.com>
2020-05-12 08:33:17 +0200

    Property fixes

--- src/main/java/it/smartcommunitylab/aac/AACUserClaimService.java
+++ src/main/java/it/smartcommunitylab/aac/AACUserClaimService.java
@@ -1,28 +1,30 @@
 package it.smartcommunitylab.aac;
 
+import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
 import org.apache.http.HttpResponse;
 import org.apache.http.HttpStatus;
 import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpDelete;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.util.EntityUtils;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import it.smartcommunitylab.aac.model.Page;
-import it.smartcommunitylab.aac.model.Service;
 import it.smartcommunitylab.aac.model.UserClaimProfile;
 
 public class AACUserClaimService {
 
 	private String aacURL;
 	
-	private ObjectMapper mapper = new ObjectMapper();
+	private ObjectMapper mapper = new ObjectMapper().configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
 
     /**
      * @param aacURL URL of the AAC service
@@ -34,20 +36,30 @@ public class AACUserClaimService {
 	}	
 	
 	@SuppressWarnings("unchecked")
-	public Page<UserClaimProfile> getServiceClaims(String token, String serviceId) throws SecurityException, AACException {
+	public Page<UserClaimProfile> getServiceClaims(String token, String serviceId, Integer page, Integer size, String sort) throws SecurityException, AACException {
 		try {
 	        final HttpResponse resp;
 	        String url = aacURL + "api/claims/" + serviceId;
-	        final HttpGet get = new HttpGet(url);
+	        String query = "?"; 
+    		if (page != null) query += "page=" + page;
+    		if (size != null) query += "&size=" + size;
+    		if (sort != null) query += "&sort=" + sort;
+    		
+	        
+	        final HttpGet get = new HttpGet(url + query);
 	        get.setHeader("Accept", "application/json");
 	        get.setHeader("Authorization", "Bearer " + token);
 	        try {
 	            resp = getHttpClient().execute(get);
 	            final String response = EntityUtils.toString(resp.getEntity());
 	            if (resp.getStatusLine().getStatusCode() == HttpStatus.SC_OK) {
-	                Page<UserClaimProfile> page = mapper.readValue(response, Page.class);
-	                page.setContent(page.getContent().stream().map(c -> mapper.convertValue(c, UserClaimProfile.class)).collect(Collectors.toList()));
-	                return page;
+	                Page<Object> p = mapper.readValue(response, Page.class);
+	                List<UserClaimProfile> list = p.getContent().stream().map(c -> mapper.convertValue(c, UserClaimProfile.class)).collect(Collectors.toList());
+	                Page<UserClaimProfile> res = new Page<>();
+	                res.setContent(list);
+	                res.setFirst(p.getFirst()); res.setLast(p.getLast()); res.setNumber(p.getNumber()); res.setNumberOfElements(p.getNumberOfElements());
+	                res.setSize(p.getSize()); res.setTotalElements(p.getTotalElements()); res.setTotalPages(p.getTotalPages());
+	                return res;
 	            }
 	            throw new AACException("Error in read service " + resp.getStatusLine());
 	        } catch (final Exception e) {
@@ -83,7 +95,7 @@ public class AACUserClaimService {
 	public UserClaimProfile getServiceClaimsForUsername(String token, String serviceId, String username) throws SecurityException, AACException {
 		try {
 	        final HttpResponse resp;
-	        String url = aacURL + "api/claims/" + serviceId +"?username=" + username;
+	        String url = aacURL + "api/claims/" + serviceId +"/username?username=" + username;
 	        final HttpGet get = new HttpGet(url);
 	        get.setHeader("Accept", "application/json");
 	        get.setHeader("Authorization", "Bearer " + token);
@@ -101,7 +113,7 @@ public class AACUserClaimService {
 			throw new AACException(e);
 		}
 	}	
-	public Service saveClaimsForUserId(String token, String serviceId, String userId, Map<String, Object> claims) throws SecurityException, AACException {
+	public UserClaimProfile saveClaimsForUserId(String token, String serviceId, String userId, Map<String, Object> claims) throws SecurityException, AACException {
 		try {
 	        final HttpResponse resp;
 	        String url = aacURL + "api/claims/" + serviceId +"/" + userId;
@@ -117,7 +129,7 @@ public class AACUserClaimService {
 	            resp = getHttpClient().execute(post);
 	            final String response = EntityUtils.toString(resp.getEntity());
 	            if (resp.getStatusLine().getStatusCode() == HttpStatus.SC_OK) {
-	                return mapper.readValue(response, Service.class);
+	                return mapper.readValue(response, UserClaimProfile.class);
 	            }
 	            throw new AACException("Error in save service " + resp.getStatusLine());
 	        } catch (final Exception e) {
@@ -126,12 +138,47 @@ public class AACUserClaimService {
 		} catch (Exception e) {
 			throw new AACException(e);
 		}
-	}		
-
-	public Service saveClaimsForUsername(String token, String serviceId, String username, Map<String, Object> claims) throws SecurityException, AACException {
+	}	
+	
+	public void deleteClaimsForUserId(String token, String serviceId, String userId) throws SecurityException, AACException {
 		try {
 	        final HttpResponse resp;
-	        String url = aacURL + "api/claims/" + serviceId +"?username=" + username;
+	        String url = aacURL + "api/claims/" + serviceId +"/" + userId;
+	        final HttpDelete delete = new HttpDelete(url);
+	        delete.setHeader("Accept", "application/json");
+	        delete.setHeader("Authorization", "Bearer " + token);
+	        try {
+	            resp = getHttpClient().execute(delete);
+	            throw new AACException("Error in save service " + resp.getStatusLine());
+	        } catch (final Exception e) {
+	            throw new AACException(e);
+	        }
+		} catch (Exception e) {
+			throw new AACException(e);
+		}
+	}	
+	public void deleteClaimsForUsername(String token, String serviceId, String username) throws SecurityException, AACException {
+		try {
+	        final HttpResponse resp;
+	        String url = aacURL + "api/claims/" + serviceId +"/username?username=" + username;
+	        final HttpDelete delete = new HttpDelete(url);
+	        delete.setHeader("Accept", "application/json");
+	        delete.setHeader("Authorization", "Bearer " + token);
+	        try {
+	            resp = getHttpClient().execute(delete);
+	            throw new AACException("Error in save service " + resp.getStatusLine());
+	        } catch (final Exception e) {
+	            throw new AACException(e);
+	        }
+		} catch (Exception e) {
+			throw new AACException(e);
+		}
+	}	
+
+	public UserClaimProfile saveClaimsForUsername(String token, String serviceId, String username, Map<String, Object> claims) throws SecurityException, AACException {
+		try {
+	        final HttpResponse resp;
+	        String url = aacURL + "api/claims/" + serviceId +"/username?username=" + username;
 	        final HttpPost post = new HttpPost(url);
 	        post.setHeader("Accept", "application/json");
 	        post.setHeader("Content-Type", "application/json");
@@ -144,7 +191,7 @@ public class AACUserClaimService {
 	            resp = getHttpClient().execute(post);
 	            final String response = EntityUtils.toString(resp.getEntity());
 	            if (resp.getStatusLine().getStatusCode() == HttpStatus.SC_OK) {
-	                return mapper.readValue(response, Service.class);
+	                return mapper.readValue(response, UserClaimProfile.class);
 	            }
 	            throw new AACException("Error in save service " + resp.getStatusLine());
 	        } catch (final Exception e) {
